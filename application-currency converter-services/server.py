24d6d142980271a044e82e322befad255458e714
Anna Erdi <erdianna607@gmail.com>
2024-05-14 22:27:32 +0200

    Changed the SOAP service to gRPC

--- application-currency converter-services/server.py
+++ application-currency converter-services/server.py
@@ -1,15 +1,26 @@
-from wsgiref.simple_server import make_server
-from spyne.server.wsgi import WsgiApplication
-from spyne.application import Application
-from spyne.protocol.soap import Soap11
-from service import CurrencyConverterService
+from concurrent import futures
+import grpc
+import currency_converter_pb2
+import currency_converter_pb2_grpc
+from ecb_data import fetch_ecb_rates, convert
 
-application = Application([CurrencyConverterService], 'urn:currency_converter',
-                          in_protocol=Soap11(validator='lxml'),
-                          out_protocol=Soap11())
+
+class CurrencyConverterService(currency_converter_pb2_grpc.CurrencyConverterServicer):
+    def ConvertCurrency(self, request, context):
+        rates = fetch_ecb_rates()
+        converted_amount = convert(request.from_currency, request.to_currency, request.amount, rates)
+        message = f"Converted {request.amount} {request.from_currency} to {converted_amount} {request.to_currency}"
+        return currency_converter_pb2.ConvertCurrencyResponse(message=message)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    currency_converter_pb2_grpc.add_CurrencyConverterServicer_to_server(CurrencyConverterService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("gRPC server starting on port 50051...")
+    server.wait_for_termination()
+
 
 if __name__ == '__main__':
-    wsgi_application = WsgiApplication(application)
-    server = make_server('0.0.0.0', 8000, wsgi_application)
-    print("SOAP server starting...")
-    server.serve_forever()
+    serve()
