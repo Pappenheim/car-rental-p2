f45a83d2f646d369715456a57e48a7142658baa0
Anna Erdi <erdianna607@gmail.com>
2024-06-02 18:13:11 +0200

    Moved the hard-coded backend url into an environment file

--- application-ui/Angular/src/app/auth/services/auth/auth.service.ts
+++ application-ui/Angular/src/app/auth/services/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-const BASIC_URL = ["https://car-rental-mono.azurewebsites.net"];
+const BASIC_URL = environment.BASIC_URL;
 
 @Injectable({
   providedIn: 'root'
---
+++ application-ui/Angular/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  BASIC_URL: "http://localhost:8080"
+  //BASIC_URL: "http://car-rental-mono.azurewebsites.net"
+};
